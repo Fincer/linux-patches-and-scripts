6c177f1f92a4a3144cc22d9cb3184920653ec5c6
Pekka Helenius <fincer89@hotmail.com>
2019-05-22 18:41:51 +0300

    Update kde-servicemenus-flifconversion

--- kde-servicemenus-flifconversion/flif-conversion.sh
+++ kde-servicemenus-flifconversion/flif-conversion.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/env bash
 
 while [ $# -gt 0 ]; do
-    OLDFORMAT=$1
-    NEWFORMAT=$(echo " $OLDFORMAT" | sed 's/\.\w*$/.flif/')
-    flif -e $OLDFORMAT $NEWFORMAT
-    shift
-done 
+  OLDFORMAT=$1
+  NEWFORMAT=$(echo " $OLDFORMAT" | sed 's/\.\w*$/.flif/')
+  flif -e $OLDFORMAT $NEWFORMAT
+  shift
+done
